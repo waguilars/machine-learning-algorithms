50f4972bef2cf86157e747e7037b334205ffa048
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-13 21:26:02 -0500

    neural network and knn

--- scripts/knn.py
+++ scripts/knn.py
@@ -1,1 +1,16 @@
 """ KNN """
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+
+x, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=0)
+
+knn = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
+predicted = knn.predict(X_test)
+acc = knn.score(X_test, y_test)
+print(acc)
+print(predicted)
--- scripts/neural-network.py
+++ scripts/neural-network.py
@@ -1,1 +1,18 @@
 """ Red neuronal """
+""" KNN """
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+x, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=0)
+
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3, 3),
+                    random_state=1, max_iter=300).fit(X_train, y_train)
+
+print(clf.predict(X_test))
+print(clf.score(X_test, y_test))
