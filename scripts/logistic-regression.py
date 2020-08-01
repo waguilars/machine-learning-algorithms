6c603719e8245e43fd6bfad087903f8562945b59
GabrielCacuango07 <gabrielcacuango07@gmail.com>
2020-07-31 19:55:58 -0500

    Logistic regresssion and data iris csv

--- scripts/logistic-regression.py
+++ scripts/logistic-regression.py
@@ -1,1 +1,46 @@
 """ Regresion logistica """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+#Análizamos los datos que tenemos disponibles
+print('Información del dataset:')
+iris = pd.read_csv("data/Iris.csv")
+#Eliminamos la primera columna ID
+iris = iris.drop('Id',axis=1)
+print(iris.head())
+#Grafico Sepal - Longitud vs Ancho
+fig = iris[iris.Species == 'Iris-setosa'].plot(kind='scatter', x='SepalLengthCm', y='SepalWidthCm', color='blue', label='Setosa')
+iris[iris.Species == 'Iris-versicolor'].plot(kind='scatter', x='SepalLengthCm', y='SepalWidthCm', color='green', label='Versicolor', ax=fig)
+iris[iris.Species == 'Iris-virginica'].plot(kind='scatter', x='SepalLengthCm', y='SepalWidthCm', color='red', label='Virginica', ax=fig)
+fig.set_xlabel('Sépalo - Longitud')
+fig.set_ylabel('Sépalo - Ancho')
+fig.set_title('Sépalo - Longitud vs Ancho')
+plt.show()
+
+#Grafico Pétalo - Longitud vs Ancho
+fig = iris[iris.Species == 'Iris-setosa'].plot(kind='scatter', x='PetalLengthCm', y='PetalWidthCm', color='blue', label='Setosa')
+iris[iris.Species == 'Iris-versicolor'].plot(kind='scatter', x='PetalLengthCm', y='PetalWidthCm', color='green', label='Versicolor', ax=fig)
+iris[iris.Species == 'Iris-virginica'].plot(kind='scatter', x='PetalLengthCm', y='PetalWidthCm', color='red', label='Virginica', ax=fig)
+fig.set_xlabel('Pétalo - Longitud')
+fig.set_ylabel('Pétalo - Ancho')
+fig.set_title('Pétalo Longitud vs Ancho')
+plt.show()
+
+#Separar todos los datos con las características y las etiquetas o resultados
+X = np.array(iris.drop(['Species'], 1))
+y = np.array(iris['Species'])
+
+#Separar los datos de "train" en entrenamiento y prueba para probar los algoritmos
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print('Son {} datos para entrenamiento y {} datos para prueba'.format(X_train.shape[0], X_test.shape[0]))
+
+#Modelo de Regresión Logística
+algoritmo = LogisticRegression()
+algoritmo.fit(X_train, y_train)
+Y_pred = algoritmo.predict(X_test)
+print('Precisión Regresión Logística: {}'.format(algoritmo.score(X_train, y_train)))
