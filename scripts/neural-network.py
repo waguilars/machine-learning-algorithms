017df12bbf012e07d2fb81062db79d1c0685ca9d
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-31 20:55:43 -0500

    wine - neural network

--- scripts/neural-network.py
+++ scripts/neural-network.py
@@ -1,32 +1,53 @@
 """ Red neuronal """
 
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris, fetch_olivetti_faces
+from sklearn.datasets import load_iris, load_wine
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import f1_score, precision_score, recall_score
+
+print('-----------------------  IRIS --------------------\n')
+
 x, y = load_iris(return_X_y=True)
 
 X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.3, random_state=0)
 
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3, 3),
-                    random_state=1, max_iter=300).fit(X_train, y_train)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    random_state=0, max_iter=1000).fit(X_train, y_train)
 
-print(clf.predict(X_test))
-print(clf.score(X_test, y_test))
+y_pred = clf.predict(X_test)
+print('Accuracy: ', clf.score(X_test, y_test))
 
-faces = fetch_olivetti_faces()
-print(faces.DESCR)
+precision = precision_score(y_test, y_pred, average=None)
+print('presicion: ', precision)
 
-x, y = fetch_olivetti_faces(return_X_y=True)
+recall = recall_score(y_test, y_pred, average=None)
+print('recall: ', recall)
+
+f1 = f1_score(y_test, y_pred, average=None)
+print('F1 Measure: ', f1)
+###################################################################
+print('-----------------------  WINE --------------------\n')
+
+x, y = load_wine(return_X_y=True)
 
 X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.3, random_state=0)
 
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3, 3),
-                    random_state=1, max_iter=300).fit(X_train, y_train)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    max_iter=1000, random_state=0).fit(X_train, y_train)
 
 
-print(clf.predict(X_test))
-print(clf.score(X_test, y_test))
+y_pred = clf.predict(X_test)
+print('Accuracy: ', clf.score(X_test, y_test))
+
+precision = precision_score(y_test, y_pred, average=None)
+print('presicion: ', precision)
+
+recall = recall_score(y_test, y_pred, average=None)
+print('recall: ', recall)
+
+f1 = f1_score(y_test, y_pred, average=None)
+print('F1 Measure: ', f1)
